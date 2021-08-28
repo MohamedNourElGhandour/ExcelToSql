0f2477b092e4010de934fb9c26938a76271ada3b
MohamedNourElGhandour <89697149+MohamedNourElGhandour@users.noreply.github.com>
2021-08-28 19:05:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ExcelToSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Click her to transfer your  data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 186);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(248, 90);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "هذه نسخة تجريبية أولا قم بتسمية قاعدة البيانات باسم  excelToTraining وتأكد ان اسم" +
+                " الsheet في الاكسل sheet1 واسم الجدول الموجود في قاعدة البيانات اسمه Book ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(522, 338);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "برنامج تجريبي ينقل بيانات من الاكسل إلى قاعدة بيانات sql server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Common;
+namespace ExcelToSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //SqlConnection sqlConnectionString = new SqlConnection(@"data source.\SQLEXPRESS;database=excelToTraining;integrated security=true");
+        SqlConnection sqlConnectionString = new SqlConnection(@"data source=.\SQLEXPRESS;database=excelToTraining;integrated security=true");
+        string m;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult g= openFileDialog1.ShowDialog();
+            if (DialogResult.OK==g)
+            {
+                 m = openFileDialog1.FileName;
+                MessageBox.Show(m);
+            }
+            #region tries
+            //SqlBulkCopy b = new SqlBulkCopy(con);
+            //con.Open();
+            //b.DestinationTableName = "excelToTraining.dbo.Query3";
+            //try
+            //{
+            //    // Write from the source to the destination.
+            //    b.WriteToServer(reader);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    // Close the SqlDataReader. The SqlBulkCopy
+            //    // object is automatically closed at the end
+            //    // of the using block.
+            //    reader.Close();
+            //}
+            #endregion
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string excelConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+m+" ;Extended Properties=Excel 8.0";
+            using (OleDbConnection connection = new OleDbConnection(excelConnectionString))
+            {
+                OleDbCommand command = new OleDbCommand("Select * FROM [Sheet1$]", connection);
+                connection.Open();
+
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    sqlConnectionString.Open(); 
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(sqlConnectionString))
+                    {
+                        bulkCopy.DestinationTableName = "Book";
+                        bulkCopy.WriteToServer(reader);
+                        MessageBox.Show("Transfer data is Succeeded");
+                    }
+
+                }
+                //DataSet ds = new DataSet();
+                //OleDbDataAdapter adap = new OleDbDataAdapter(command);
+                //adap.Fill(ds);
+                //dataGridView1.DataSource = ds;
+                ////dataGridView1.
+            }
+        }
+    }
+}
